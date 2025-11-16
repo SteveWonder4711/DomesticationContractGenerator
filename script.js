99086621a6355e5e49de025e9def25f76020eeb8
Charlotte Abromeit <charlotte.abromeit@web.de>
2025-11-16 21:20:34 +0100

    affini pronouns!!

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
+let personalpronoun = "she";
+let possesivepronoun = "her";
+
+function startup() {
+  pronounSelect();
+  generateContract();
+}
+
+
+function pronounSelect() {
+  var pronounlist = document.getElementById("affinipronouns");
+  var pronouns = pronounlist.selectedOptions[0].value;
+  if (pronouns != "custom") {
+    document.getElementById("custompronouns").style.display = "none";
+    personalpronoun = pronouns.split(" ")[0];
+    possesivepronoun = pronouns.split(" ")[1];
+    generateContract();
+  } else {
+    document.getElementById("custompronouns").style.display = "block";
+    customPronouns();
+  }
+}
+
+function customPronouns() {
+  personalpronoun = document.getElementById("personalpronoun").value;
+  possesivepronoun = document.getElementById("possesivepronoun").value;
+  generateContract();
+}
+
 function generateContract() {
   var ownername = document.getElementById("affininame").value;
   var petname = document.getElementById("petname").value;
   var petfirstname = petname.split(" ")[0];
 
   var ownerfillins = document.querySelectorAll(".affinifillin");
-  var i;
 
   for (i = 0; i < ownerfillins.length; i += 1) {
     ownerfillins[i].innerHTML = ownername;
   }
 
   var petfillins = document.querySelectorAll(".petfillin");
-  var i;
 
   for (i = 0; i < petfillins.length; i += 1) {
     petfillins[i].innerHTML = petname;
   }
 
   var petfirstfillins = document.querySelectorAll(".petfirstfillin");
-  var i;
 
   for (i = 0; i < petfirstfillins.length; i += 1) {
     petfirstfillins[i].innerHTML = petfirstname;
   }
+
+  var affinipersonals = document.querySelectorAll(".affinipersonal");
+
+  for (i = 0; i < affinipersonals.length; i += 1) {
+    affinipersonals[i].innerHTML = personalpronoun;
+  }
+
+  var affinipossesive = document.querySelectorAll(".affinipossesive");
+
+  for (i = 0; i < affinipossesive.length; i += 1) {
+    affinipossesive[i].innerHTML = possesivepronoun;
+  }
+
+
 
   var additionalterms = [];
 
@@ -67,3 +107,5 @@ function generateContract() {
 function downloadContract() {
   window.print();
 }
+
+
